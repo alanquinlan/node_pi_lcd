2d018cdf8ddd4717ce4cd68678c6ea44fa08c2a2
Alan <alan@epicenterconsulting.com>
2016-07-06 22:24:36 -0500

    bootstrap, jquery, handlebars, cleanup

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exphbs  = require('express-handlebars');
 
 
 
@@ -29,7 +30,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 
 
